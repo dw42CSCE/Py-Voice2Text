03ec6df081e5fbab99619e814f4598241ca6eb39
dw42CSCE <dw42@email.sc.edu>
2024-09-23 13:06:54 -0400

    Working on allowing replay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#Copyright 2024 Dallas Wade
-#Voice to txt script in python using WitAI Speech Recognition
+# Copyright 2024 Dallas Wade
+# Voice to txt script in python using WitAI Speech Recognition
 # REQUIRES INTERNET CONNECTION
 
 import speech_recognition as sr
@@ -7,26 +7,25 @@ import pyttsx3
 import os
 from dotenv import load_dotenv
 
-#Reading in needed files
+#Reading in needed intro and help files
 with open("intro.txt", "r") as file:
     introtext = file.read()
 with open("helptext.txt", "r") as file:
     helptext = file.read()
 
-#Initializing recognizer, loading and creating variable for environ variable WIT_API_KEY
+# Initializing recognizer, loading and creating variable for environ variable WIT_API_KEY
 r = sr.Recognizer()
 load_dotenv()
 key = os.getenv('API_KEY')
 
-#Printing introdution
+# Printing introdution
 print(introtext)
 
-#Records text from source
+# Records text from source
 def record_text():
 
     while(1):
         try:
-
             with sr.Microphone() as source2:
                 
                 r.adjust_for_ambient_noise(source2, duration=0.2)
@@ -34,6 +33,7 @@ def record_text():
 
                 Text = r.recognize_wit(audio2, key)
 
+                # Checks for help request
                 if Text.lower() == "help": 
                     print(helptext)
                 
@@ -46,6 +46,7 @@ def record_text():
 
     return
 
+# Outputs text into a file
 def output_text(text, f):
 
     f.close()
@@ -54,51 +55,64 @@ def output_text(text, f):
     f.write("\n")
     f.close()
 
-print("Do you want to overwrite a file or add to one?\n1. Overwrite \n2. Add" )
-
-askagain = True
-
-while askagain:
-    choice = record_text()
-    if choice.lower() == "one":
-        writetype = "w"
-        askagain = False
-
-    elif choice.lower() == "two":
-        writetype = "a"
-        askagain = False
-
-    else: print("Please say \"One\" or \"Two\"\nDo you want to overwrite a file or add to one?\n1. Overwrite \n2. Add")
-
-print("What is the name of the file you would like to alter?")
-
-filename = record_text().lower()
-file = filename + ".txt"
-
 running = True
 
-while file == "helptext.txt" or file == "help.txt": 
+while(running):
+    print("Do you want to overwrite a file or add to one?\n1. Overwrite \n2. Add" )
+
+    askagain = True
+
+    # Repeats question until user says acceptable choice
+    while askagain:
+        choice = record_text()
+        if choice.lower() == "one":
+            writetype = "w"
+            askagain = False
+
+        elif choice.lower() == "two":
+            writetype = "a"
+            askagain = False
+
+        elif choice.lower() == "restart":
+            break
+
+        else: print("Please say \"One\" or \"Two\"\nDo you want to overwrite a file or add to one?\n1. Overwrite \n2. Add")
+
     print("What is the name of the file you would like to alter?")
+
     filename = record_text().lower()
+
+    if choice.lower() == "restart":
+        break
+
     file = filename + ".txt"
 
-f = open(file, writetype)
+    # Ensures user doesnt alter helptext file, 
+    while file == "helptext.txt" or file == "help.txt": 
+        print("What is the name of the file you would like to alter?")
+        filename = record_text().lower()
 
-print("Say \"Quit\" to stop the program")
+        if choice.lower() == "restart":
+            break
 
-while(running):
+        file = filename + ".txt"
 
-    text = record_text()
-    
+    f = open(file, writetype)
 
-    if text.lower() == "quit": 
-        print("Closing")
-        running = False
+    print("Say \"Quit\" to stop the program")
 
-    elif text.lower() == "help": 
-        print(helptext)
+    # Running loop, saying Quit changes running value and stops program
+    while(running):
 
-    else:
-        output_text(text, f)
-        print("Wrote text")
+        text = record_text()
+        
+        if text.lower() == "quit": 
+            print("Closing")
+            running = False
 
+        elif text.lower() == "help": 
+            print(helptext)
+
+        else:
+            output_text(text, f)
+            print("Wrote text")
